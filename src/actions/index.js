fac436548bf349fe183e3d6b3a33603e4cf0fc9e
Administrador <administrador@MacBook-Pro-de-Administrador.local>
2019-05-17 08:07:37 -0400

    Set up guessWord tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const actionTypes = {
 	CORRECT_GUESS: 'CORRECT_GUESS',
+	GUESS_WORD: 'GUESS_WORD'
 }
 
 /**
@@ -8,4 +9,18 @@ export const actionTypes = {
 */
 export function correctGuess(){
 	return { type: actionTypes.CORRECT_GUESS };	
+}
+
+/**
+	* Returns Redux Thunk function that dispatches GUESS_WORD action
+		and conditionally CORRECT_GUESS action
+	* @function guessWord
+	* @param {string} guessedWord - Guessed word
+	* @returns {function} - Redux Thunk function 
+*/
+
+export const guessWord = (guessWord) => {
+	return function(dispatch, getState){
+
+	}
 }
---
+++ src/reducers/guessedWordReducer.js
@@ -0,0 +1,9 @@
+/**
+	* @function guessedWordReducer
+	* @param {array} state - Array of guessed words.
+	* @params {object} action - action to be reduced.
+	* @returns {array} - new guessedWrods state
+*/
+export default (state, action) => {
+	return null;
+}
